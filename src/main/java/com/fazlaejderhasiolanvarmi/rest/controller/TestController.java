5dd3693e7c27645e3ce5459048ef479dbcd28264
fazlaejderhasiolanvarmi <burakceltik@hotmail.com>
2022-07-14 03:33:17 +0300

    Added Latifi

--- src/main/java/com/fazlaejderhasiolanvarmi/rest/controller/TestController.java
+++ src/main/java/com/fazlaejderhasiolanvarmi/rest/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.fazlaejderhasiolanvarmi.rest.controller;
 
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/formula")
 public class TestController {
     //formula/driver?name=
-    @GetMapping("/driver")
+    @GetMapping("/drivers")
     public ResponseEntity<?> getPilot(@RequestParam String name){
 
-        if(name.equals("Charles")){
+        if(name.equals("charles")){
             return ResponseEntity.ok().body("2022 World Champion!!");
+        }else if(name.equals("latifi")){
+            return new ResponseEntity("Latifi Çok Kaza Yapıyo", HttpStatus.PAYMENT_REQUIRED);
         }
 
         return ResponseEntity.badRequest().body("Bunlar Tırt");
